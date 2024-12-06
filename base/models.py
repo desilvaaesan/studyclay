b375cf68d1370b2bc6dae745f025ba4bdd0bc3ee
desilvaaesan <aesanclyde.desilva@gmail.com>
2024-12-06 14:44:15 +0800

    Chore: arrange order from latest to oldest

--- base/models.py
+++ base/models.py
@@ -15,9 +15,11 @@ class Room(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants = 
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-
+    
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
